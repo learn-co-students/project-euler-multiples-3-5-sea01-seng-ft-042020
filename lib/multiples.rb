7997d257495fe3fc2323ba33fc30d3631e73177b
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-08-19 13:11:33 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    multiples = []
+    for i in 1...limit
+        multiples.push(i) if i%3 == 0 || i%5 == 0
+    end
+    return multiples
+end
+
+def sum_multiples(limit)
+    return collect_multiples(limit).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+  
+class Multiples
+    attr_reader :limit, :multiples
+  
+    def initialize(limit)
+      @limit = limit
+      @multiples = collect_multiples
+    end
+  
+    def collect_multiples
+        multiples = []
+        for i in 1...limit
+            multiples.push(i) if i%3 == 0 || i%5 == 0
+        end
+        return multiples
+    end
+    
+    def sum_multiples
+        return collect_multiples.sum
+    end
+
+  end
