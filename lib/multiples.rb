2fbf8af0a18b0640b0d08615a61f09f4a5bf2ed9
sap2411 <steven.parsons.us@gmail.com>
2020-09-28 16:42:07 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+    ar = []
+    multOf3 = 1
+    multOf5 = 1
+    while (multOf3) * 3 < limit || (multOf5) * 5 < limit
+        if (multOf3) * 3 === (multOf5) * 5
+            ar.push(multOf3 * 3)
+            multOf3 += 1
+            multOf5 += 1
+        elsif multOf3 * 3 < multOf5 * 5
+            ar.push(multOf3 * 3)
+            multOf3 += 1
+        else
+            ar.push(multOf5 * 5)
+            multOf5 += 1
+        end
+    end
+    return ar
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).reduce(0) {|sum, n| sum += n}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,31 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        ar = []
+        multOf3 = 1
+        multOf5 = 1
+        while (multOf3) * 3 < self.limit || (multOf5) * 5 < self.limit
+            if (multOf3) * 3 === (multOf5) * 5
+                ar.push(multOf3 * 3)
+                multOf3 += 1
+                multOf5 += 1
+            elsif multOf3 * 3 < multOf5 * 5
+                ar.push(multOf3 * 3)
+                multOf3 += 1
+            else
+                ar.push(multOf5 * 5)
+                multOf5 += 1
+            end
+        end
+        return ar
+    end
+    
+    def sum_multiples()
+        self.collect_multiples().reduce(0) {|sum, n| sum += n}
+    end
+end
